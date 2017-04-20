1bbd4762cf9336bbfb4acbc96482fcef9a7c98da
Elena <not_to_hate@mail.ru>
2017-04-20 19:43:54 +0700

    no message

---
+++ MainForm/MainForm/Form1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MainForm
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+            DataTable table = new DataTable();
+            table.Columns.Add("Угол", typeof(float));
+            table.Columns.Add("Интенсивность", typeof(float));
+            table.Columns.Add("S11", typeof(float));
+            table.Columns.Add("S33", typeof(float));
+            table.Columns.Add("S34", typeof(float));
+
+            DGV.DataSource = table;
+
+        }
+
+        private void CalculationButton_Click(object sender, EventArgs e)
+        {
+            var wavelength = IntialDateControl.SomeValue.Text;
+        }
+    }
+}
